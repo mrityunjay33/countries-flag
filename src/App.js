cbf560a38cbe5796786bd8bbd4eaceb994c45b47
Mrityunjay Kumar <mk10399@gmail.com>
2023-12-25 13:04:33 +0530

    Countries-flag

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [flags, setFlags] = useState([]);
+
+  useEffect(()=>{
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try{
+      const res = await fetch('https://restcountries.com/v3.1/all');
+      const data = await res.json();
+      setFlags(data);
+      console.log(data);
+    }
+    catch(err){
+      console.log(err);
+      return null;
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {flags.map((data, idx) => <div key={idx}>
+        <div className='card'>
+          <div className='card-content'>
+          <img className='image' src={data.flags.png} alt={`Flag of ${data.name.common}`} />
+          <div className='country-name'>{data.region}</div>
+          </div>
+        </div>
+        </div>)}
     </div>
   );
 }
